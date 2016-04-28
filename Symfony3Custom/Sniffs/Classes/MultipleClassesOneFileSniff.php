1eeb72af7de4ecb89750eec34c1305373d5c42d1
Karin van den Berg <karin@endouble.com>
2016-04-28 10:30:59 +0200

    Rename standard

---
+++ Symfony3Custom/Sniffs/Classes/MultipleClassesOneFileSniff.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file is part of the Symfony3Custom-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  Symfony3Custom-coding-standard
+ * @author   Authors <Symfony3Custom-coding-standard@escapestudios.github.com>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @link     https://github.com/escapestudios/Symfony3Custom-coding-standard
+ */
+
+/**
+ * Throws errors if multiple classes are defined in a single file.
+ *
+ * PHP version 5
+ *
+ * Symfony coding standard specifies: "Define one class per file;"
+ *
+ * @category PHP
+ * @package  Symfony3Custom-coding-standard
+ * @author   Authors <Symfony3Custom-coding-standard@escapestudios.github.com>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @link     https://github.com/escapestudios/Symfony3Custom-coding-standard
+ */
+class Symfony3Custom_Sniffs_Classes_MultipleClassesOneFileSniff
+    implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * The number of times the T_CLASS token is encountered in the file.
+     *
+     * @var int
+     */
+    protected $classCount = 0;
+
+    /**
+     * The current file this class is operating on.
+     *
+     * @var string
+     */
+    protected $currentFile;
+
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+        'PHP',
+    );
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_CLASS);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile All the tokens found in the document.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        if ($this->currentFile !== $phpcsFile->getFilename()) {
+            $this->classCount  = 0;
+            $this->currentFile = $phpcsFile->getFilename();
+        }
+
+        $this->classCount++;
+
+        if ($this->classCount > 1) {
+            $phpcsFile->addError(
+                'Multiple classes defined in a single file',
+                $stackPtr
+            );
+        }
+
+        return;
+    }
+}
---
+++ Symfony3Custom/Tests/Formatting/BlankLineBeforeReturnUnitTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the Symfony3Custom-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  Symfony3Custom-coding-standard
+ * @author   Authors <Symfony3Custom-coding-standard@escapestudios.github.com>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @link     https://github.com/escapestudios/Symfony3Custom-coding-standard
+ */
+
+/**
+ * Unit test class for the BlankLineBeforeReturn sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  Symfony3Custom-coding-standard
+ * @author   Authors <Symfony3Custom-coding-standard@escapestudios.github.com>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @link     https://github.com/escapestudios/Symfony3Custom-coding-standard
+ */
+class Symfony3Custom_Tests_Formatting_BlankLineBeforeReturnUnitTest
+    extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList()
+    {
+        return array(
+            37 => 1
+        );
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array();
+    }
+}
