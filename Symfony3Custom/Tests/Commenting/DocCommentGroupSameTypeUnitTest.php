3d782c2784e874f476f903bfa190d44da2a3b0e2
Vincent Langlet <vincentl@theodo.fr>
2017-04-18 00:22:59 +0200

    :sparkles: Comment should be group by type and separate by one blank line

---
+++ Symfony3Custom/Tests/Commenting/DocCommentGroupSameTypeUnitTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Unit test class for the DocCommentGroupSameType sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ */
+class Symfony3Custom_Tests_Commenting_DocCommentGroupSameTypeUnitTest
+    extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return array(
+            5 => 1,
+            7 => 1,
+            8 => 1,
+            11 => 1,
+            12 => 1,
+            13 => 1,
+            18 => 1,
+            27 => 1,
+            31 => 1,
+        );
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    protected function getWarningList()
+    {
+        return array();
+    }
+}
