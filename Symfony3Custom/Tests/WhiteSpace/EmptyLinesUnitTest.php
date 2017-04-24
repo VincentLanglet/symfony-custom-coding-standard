876f9008c48271764f0084937d340c73e5138c36
Vincent Langlet <vincentl@theodo.fr>
2017-04-24 21:02:00 +0200

    :sparkles: Add check for multiple blank line

---
+++ Symfony3Custom/Tests/WhiteSpace/EmptyLinesUnitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Unit test class for the EmptyLines sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ */
+class Symfony3Custom_Tests_WhiteSpace_EmptyLinesUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return array(
+            5 => 1,
+            6 => 1,
+        );
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    protected function getWarningList()
+    {
+        return array();
+    }
+}
