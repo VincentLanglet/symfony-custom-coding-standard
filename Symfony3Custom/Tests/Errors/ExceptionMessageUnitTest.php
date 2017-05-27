7663364055a0786727ac2bc55a5d824730741b18
Vincent Langlet <vincentl@theodo.fr>
2017-05-27 16:22:54 +0200

    :sparkles: Add Error/Deprecated messages sniffs

---
+++ Symfony3Custom/Tests/Errors/ExceptionMessageUnitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Symfony3Custom\Tests\Errors;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the ExceptionMessage sniff.
+ *
+ * @group Symfony3Custom
+ */
+class ExceptionMessageUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return array(
+            17 => 1,
+        );
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array();
+    }
+}
