31b0423bec5e5a87ec30399ddf222ba0a7a8a03f
Vincent Langlet <vincentlanglet@hotmail.fr>
2019-12-22 15:37:16 +0100

    :sparkles: Improve UnusedUseSniff

--- SymfonyCustom/Tests/Namespaces/UnusedUseUnitTest.php
+++ SymfonyCustom/Tests/Namespaces/UnusedUseUnitTest.php
@@ -22,8 +22,24 @@ class UnusedUseUnitTest extends AbstractSniffUnitTest
     public function getErrorList()
     {
         return [
-            11 => 1,
-            16 => 1,
+            12  => 1,
+            13  => 1,
+            14  => 1,
+            15  => 2,
+            16  => 2,
+            18  => 1,
+            20  => 1,
+            21  => 1,
+            22  => 1,
+            23  => 1,
+            93  => 1,
+            95  => 1,
+            124 => 1,
+            125 => 1,
+            126 => 1,
+            127 => 1,
+            128 => 1,
+            129 => 1,
         ];
     }
 
